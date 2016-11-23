64e3ddc40ea7c638019cb468b08e41bfc7dea530
ttphong <ttphong2608@gmail.com>
2016-11-23 21:32:16 +0700

    Creating an enemy

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		GameObject obj = Instantiate (enemy, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
+		obj.transform.parent = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
