846a947821f7c607c15b1b7bbce3e8cb49c07393
ttphong <ttphong2608@gmail.com>
2016-11-23 21:02:43 +0700

    Restring the spaceship position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,20 @@ public class PlayerController : MonoBehaviour {
 
 	public float seed = 15f;
 
-	// Use this for initialization
-	void Start () {
-	
+	float xmin;
+	float xmax;
+	float padding = 1f;
+
+	void Start() {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+
+		// calculate xmax and xmin
+		xmax = rightMost.x - padding;
+		xmin = leftMost.x + padding;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 /*		if (Input.GetKey (KeyCode.UpArrow)) {
 			MovePlayerShip (0f, seed);
@@ -24,9 +32,13 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void MovePlayerShip(float dx, float dy) {
-		Vector3 deltaPos = new Vector3 (dx, dy, 0f);
+		Vector3 deltaPos = new Vector3 (dx, dy, transform.position.z);
 		//print (newPos);
 
 		this.transform.position += deltaPos * Time.deltaTime;
+
+		float xnew = Mathf.Clamp (this.transform.position.x, xmin, xmax);
+
+		this.transform.position = new Vector3 (xnew, transform.position.y, transform.position.z);
 	}
 }
