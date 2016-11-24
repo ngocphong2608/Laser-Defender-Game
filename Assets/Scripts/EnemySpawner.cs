2d38c06cc395307075ec8cd7bc36a1dd0c4bbeea
ttphong <ttphong2608@gmail.com>
2016-11-24 21:39:45 +0700

    Moving the enemy formation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,17 +4,56 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemy;
+	public float width = 10f;
+	public float height = 5f;
+
+	float xmax, xmin;
+	float padding = 0f;
+	bool moveRight = true;
+	float seed = 5f;
 
 	// Use this for initialization
 	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance));
+		
+		// calculate xmax and xmin
+		xmax = rightMost.x - padding;
+		xmin = leftMost.x + padding;
+
 		foreach (Transform child in transform) {
 			GameObject obj = Instantiate (enemy, child.position, Quaternion.identity) as GameObject;
 			obj.transform.parent = child;
 		}
 	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube (transform.position, new Vector3 (width, height));
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (moveRight) {
+			transform.position += Vector3.right * Time.deltaTime * seed;
+		} else {
+			transform.position += Vector3.left * Time.deltaTime * seed;
+		}
+
+		float leftEdge = transform.position.x - width / 2;
+		float rightEdge = transform.position.x + width / 2;
+
+		if (leftEdge < xmin || rightEdge > xmax) {
+			moveRight = !moveRight;
+		}
+	}
+
+	void MoveEnemy (Transform child)
+	{
+		float xnew = child.position.x - 1;
+		if (xnew < -width / 2) {
+
+		}
 	}
 }
