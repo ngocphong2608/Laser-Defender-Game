53a9acf13c91cd05c7ecb95801d8f2f1bd3874db
ttphong <ttphong2608@gmail.com>
2016-11-25 08:55:45 +0700

    Shooting Enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public float health = 150f;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		Laser missile = col.gameObject.GetComponent<Laser> ();
+		if (missile) {
+			health -= missile.GetDamage ();
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+			missile.Hit();
+		}
+	}
+}
