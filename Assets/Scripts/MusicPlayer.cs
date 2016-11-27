6276f3c2bc987aaa9bf2c283fe20b933f04399bf
ngocphong2608 <tranthanhphong260895@gmail.com>
2016-11-27 21:02:44 +0700

    26. Polishing The Menu System II

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,23 +5,45 @@ public class MusicPlayer : MonoBehaviour {
 
 	static MusicPlayer instance = null;
 
-	void Awake () {
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+
+	AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+		music = GetComponent<AudioSource> ();
+
 		if (instance != null) {
 			Destroy (gameObject);
 			//instance = null;
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad (gameObject);
+
+			music.clip = startClip;
+			music.loop = true;
+			music.Play ();
 		}
 	}
 
-	// Use this for initialization
-	void Start () {
+	void OnLevelWasLoaded(int level) {
+		Debug.Log ("Level was loaded: " + level);
+		if (music == null)
+			return;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		music.Stop ();
+
+		if (level == 0) {
+			music.clip = startClip;
+		} else if (level == 1) {
+			music.clip = gameClip;
+		} else if (level == 2) {
+			music.clip = endClip;
+		}
+
+		music.loop = true;
+		music.Play ();
 	}
 }
